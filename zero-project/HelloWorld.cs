fd8936bf7b23891453698a1934b6987b9c11f221
Ruslan Sydir <rsydir@gmail.com>
2023-08-26 09:53:38 +0200

    Leap Year Check

--- zero-project/HelloWorld.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class HelloWorld {
-
-    public static void Main()
-    {
-        Console.WriteLine("Hello World");
-    }
-}
---
+++ zero-project/LeapYearChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace zero_project
+{
+	public class LeapYearChecker
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Enter year: ");
+			int year = Convert.ToInt32(Console.ReadLine());
+
+			bool f;
+
+			if (year % 400 == 0) f = true;
+			else
+			{
+				if (year % 100 == 0) f = false;
+				else
+				{
+					if (year % 4 == 0) f = true;
+					else f = false;
+				}
+			}
+
+			switch (f)
+			{
+				case true:
+					Console.WriteLine($"{year} is leap");
+					break;
+				case false:
+					Console.WriteLine($"{year} isn`t leap");
+					break;
+			}
+		}
+	}
+}
+
